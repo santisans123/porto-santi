642b1d19eb28ead16994b5f0b5662fb004d9b4d0
santisans123 <santitok21@gmail.com>
2024-11-12 23:17:45 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Porto Github
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/santisans123"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Santi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
